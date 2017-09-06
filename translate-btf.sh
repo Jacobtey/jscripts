882ee369c503a43a71d3e97484e9442530efa3cc
Jacobtey <jacobtey@gmail.com>
2017-09-06 23:01:08 +0300

    Create translate-btf.sh

---
+++ translate-btf.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Сценарий цепляется на горячие клавиши и по их нажатию
+# с помощью translate-shell (https://github.com/soimort/translate-shell)
+# переводит выделенный текст с русского на английский или с любого языка на русский
+# и выводит перевод во всплывающем окне, одновременно добавляя его в /tmp/trans.txt
+
+# The script bind to hotkeys translates by pressing them the selected text via translate-shell (https://github.com/soimort/translate-shell) from Russian into English or from any language into Russian and displays the translation in a pop-up window, while adding it to /tmp/trans.txt 
+
+#Arch based: $ yaourt -S translate-shell libnotify xclip 
+
+    notify-send -i dialog-question "Ищу перевод..." " "
+
+	TEXT=$(xclip -selection primary -o)
+	IDENT=$(translate-shell -identify "$TEXT" | grep Code | grep ru)
+	
+	if [ "$IDENT" ]; then
+	
+	    TRANS=$(translate-shell -b -t en "$TEXT" -output $HOME/.trans.txt)
+	
+	else
+	
+	    TRANS=$(translate-shell -b "$TEXT" -output $HOME/.trans.txt)
+	
+	fi
+	
+	TRAX=$(cat $HOME/.trans.txt)
+	TRAN=$(echo -e "$TRAX \n" >> /tmp/trans.txt)
+
+	cp /dev/null $HOME/.trans.txt
+	$TRANS
+	
+	if [ "$TRAX" ]; then
+	  
+	   notify-send -i dialog-information "ПЕРЕВОД:" "$TRAX"
+	   $TRAN
+	   	   
+	else
+	
+	    notify-send -i dialog-error -t 7000 "ОШИБКА:" "Сервис не доступен"
+	    
+	fi
+	
+     
+
